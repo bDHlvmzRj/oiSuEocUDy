ad3a9efec8e9ebf4639f3ac8aaada55457b1cd8c
bDHlvmzRj <CiLFd6ZNs5HCvr55@gmail.com>
2025-01-15 21:51:55 +0900

    Delete ec2 directory

--- ec2/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "workbooks" {
-  description = "Mapping of workbook and sheet configurations"
-  type = map(map(object({
-    aws_instance = object({
-      ami           = string
-      instance_type = string
-    })
-  })))
-}
